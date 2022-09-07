ad0e039d078385a54da7013d81cce131250e7a20
Autif Kamal <kautif@gmail.com>
2022-09-06 20:33:49 -0500

    started Player 1 side

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
 import GameBoard from './components/GameBoard';
-import DiceBoards from './components/DiceBoards';
-import DiceBoardP1 from './components/DiceBoardP1';
-import DiceBoardP2 from './components/DiceBoardP2';
-import Player1 from './components/Player1';
-import Player2 from './components/Player2';
 
 function App() {
   return (
--- src/components/Player1.js
+++ src/components/Player1.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default function Player1 () {
     return (
-        <div>
-            Player 1
+        <div className="knucklebones__p1">
+            <img className="p1__pp" src="https://upload.wikimedia.org/wikipedia/en/8/8d/Zero-mmx.png" alt="player 1 image" />
+            <h1>Player 1</h1>
+            <h2>(Player 1 Score)</h2>
+            <div className="p1__diceholder">Dice Holder</div>
         </div>
     )
 }
